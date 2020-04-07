588bf8358a6f98aec502e125963ce2ff3aa8a6b2
Rene Gens <renegens@gmail.com>
2020-04-07 12:46:33 +0300

    Add analytics id

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,12 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-162898200-1",
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
